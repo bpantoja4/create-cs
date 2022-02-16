a139d1595972d87c57a86dc3a7049b16709265ef
bpantoja4 <91565762+bpantoja4@users.noreply.github.com>
2022-02-16 14:05:38 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let listFood: Image[] = []
+let listAnimal: Image[] = []
 let mySprite: Sprite = null
 let response = ""
 function checkScore () {
@@ -7,6 +9,238 @@ function checkScore () {
     }
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    listFood = [
+    img`
+        ...........ccccc66666...........
+        ........ccc4444444444666........
+        ......cc444444444bb4444466......
+        .....cb4444bb4444b5b444444b.....
+        ....eb4444b5b44444b44444444b....
+        ...ebb44444b4444444444b444446...
+        ..eb6bb444444444bb444b5b444446..
+        ..e6bb5b44444444b5b444b44bb44e..
+        .e66b4b4444444444b4444444b5b44e.
+        .e6bb444444444444444444444bb44e.
+        eb66b44444bb444444444444444444be
+        eb66bb444b5b44444444bb44444444be
+        fb666b444bb444444444b5b4444444bf
+        fcb666b44444444444444bb444444bcf
+        .fbb6666b44444444444444444444bf.
+        .efbb66666bb4444444444444444bfe.
+        .86fcbb66666bbb44444444444bcc688
+        8772effcbbbbbbbbbbbbbbbbcfc22778
+        87722222cccccccccccccccc22226678
+        f866622222222222222222222276686f
+        fef866677766667777776667777fffef
+        fbff877768f86777777666776fffffbf
+        fbeffeefffeff7766688effeeeefeb6f
+        f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+        f66ddfffffeeeffeffeeeeeffeedb46e
+        .c66ddd4effffffeeeeeffff4ddb46e.
+        .fc6b4dddddddddddddddddddb444ee.
+        ..ff6bb444444444444444444444ee..
+        ....ffbbbb4444444444444444ee....
+        ......ffebbbbbb44444444eee......
+        .........fffffffcccccee.........
+        ................................
+        `,
+    img`
+        .............beebbbb............
+        ............eebbbb4bb...........
+        ............eb344bb4bb..........
+        ............e44334bb4bb.........
+        ............eb433344b4be........
+        ............4eb43344444be.......
+        ...........bd4eb43333344bb......
+        ..........b455d4443333444bb.....
+        ..........4d5555d444333444bb....
+        .........4555555dd4b4443444be...
+        ........bd5555d555d4bb444444ee..
+        ........b55ddd665555bb4b44444ee.
+        .......bd5555677655554ebb44444eb
+        .......43222558855555d4eeb44b4ee
+        ......b422332ddd555222d4eebbb4be
+        ......be22232ed55522332db4ebbbbe
+        .....bde22222e555e22232edd4bbbbe
+        .....b52e222e3555e22222eddd4ebee
+        ....bd552eee355552e222e355544eee
+        ....665dd5555555552eee355dd4deee
+        ...6776555555555555555551554d4ee
+        ...4885222555dddd6655551544d4eee
+        ..b45522332555dd677611d444ddeee.
+        ..4d5222232e55555881d44ddd4eee..
+        .bdd5e22222e555115114d54d4ee....
+        .b55d2e222e351144d1d55eeee......
+        bd5ddd2eee3d444555dd4e..........
+        b555115dddd55d544eede...........
+        4511d444d5544ee...4de...........
+        41d4555d4ee........44...........
+        41554eede.......................
+        44ee...4e.......................
+        `,
+    img`
+        ......................bbb.......
+        ....................bb333b......
+        .................bbb333d33b.....
+        ................bb333333d3a.....
+        ..............bb33332eeeedba....
+        ............bbb333323eee2e3a....
+        ..........bbd333333e22222ed3a...
+        .......bbbdd3333333e22222edda...
+        ......bb3d333333333be222eb3d3a..
+        ...bbb3dd33333333333beeeb33d3a..
+        ..b3ddd33333333333333333333dda..
+        bbddd3333333333333333333333dd3a.
+        b33dddddd3333333333333333333d3a.
+        bb3333333ddddd33333333333333dda.
+        bbbbbbb333dd33dddddddddd3333ddba
+        b55553bbbbbb3333dd33333ddd33dd3a
+        b555555555553bbbbbbbb33333dddd3a
+        bd555555555555555dddbaaaaab3d3ba
+        bb55555555555555555dddddddbb33ba
+        b3bb35555555555d5555d55dddddbbba
+        b33333bbb355dd55555d555ddddddbba
+        b5555d333333bbb35dddddd55ddddbba
+        b5d555dd5553333bbbbb3ddddddddb3a
+        b5d555555555555dd3333bbbbbb3db3a
+        bd5d555555d55555dd555ddbbbbbbb3a
+        bbb55dd555555555555555ddddddbb3a
+        ...bbbbdd555ddd5555ddddddddddb3a
+        .......bbbb555555d5ddd5ddddddb3a
+        ...........bbbb55555555555dd533a
+        ...............bbbbddd5d55d5b3ba
+        ...................bbbbddddb3ba.
+        .......................bbbaaaa..
+        `,
+    img`
+        ..............bbbbbbb...........
+        ...........bb66663333baa........
+        .........bb3367776333663aa......
+        ........b33333888333389633aa....
+        .......b3333333333333389633aa...
+        ......b34443333333333338633bae..
+        .....b3455433333333334443333ae..
+        ....b33322333dddd3333455233daee.
+        ...b3d333333dd3bbbb33322333dabe.
+        ..b3d333333d3bb33bb33333333da4e.
+        ..bd33333333b33aab3333333223a4ee
+        .b3d3663333b33aab33366332442b4ee
+        .bd3b983333a3aa3333387633ee3b4ee
+        .bd6983333baaa333333387633bb4bee
+        b3d6833333bba333333333863ba44ebe
+        bdd3333333bb3333333333333a44bebe
+        add666633333322333366333ba44bbbe
+        ad67776333332442336983d3a444b4e.
+        add888b333333ee3369833d3a44b44e.
+        add333333333333336833d3a444b4e..
+        a3dd3333344433333dddd3a444b44e..
+        ab33ddd325543333dd33aa444b44e...
+        .eabb3dd32233333baaa4444b44e....
+        .ebabb3d333d33baa444443b44e.....
+        ..ebaab3ddd3aaa4444433b44e......
+        ..eebbaab33a44444333b444e.......
+        ...eeebbaab444b333b4444e........
+        ....ebeeebbbbbbbb4444ee.........
+        .....eebbbb44444444ee...........
+        .......eeebbb444eee.............
+        ..........eeeeee................
+        ................................
+        `
+    ]
+    listAnimal = [
+    img`
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . c 4 d 5 f 1 d 5 b b . . . . 
+        b 4 4 4 d d f f 1 5 5 b . . . . 
+        . b 4 4 4 4 b f d 5 5 b . . . . 
+        . . b 4 4 4 4 5 5 5 5 d b . . . 
+        . . b 5 5 5 5 5 5 5 5 d d b . . 
+        . b 5 5 5 5 5 5 5 5 d d d d b . 
+        . b 5 5 5 5 5 5 5 b b b d d d b 
+        . b 5 5 5 5 5 5 c d 5 5 b d d c 
+        . b 5 5 5 5 5 5 d c d 5 d b b c 
+        . b d 5 5 5 5 5 d d c b 5 5 b . 
+        . . b b 5 5 5 d d d d c c c b b 
+        . . . b b c c c c c c c c . . . 
+        `,
+    img`
+        . . . . . . . . . . . . . . . . 
+        . . e f . . . . . . f e . . . . 
+        . . e e f . . . . f e e . . . . 
+        . . f e f e e e e e e f . . . . 
+        . . f e e e e e e e e f . . . . 
+        . . e e e e e e e e e e . . . . 
+        . . e e f e e e e f e e . . . . 
+        . . f e e e e e e e e f . . f f 
+        . . f f e e f f e e 6 f . f e f 
+        . . . f 6 6 6 6 6 6 f f f e e f 
+        . . . f f e e e e e e f f e f . 
+        . . . f e e e e e f e e f f . . 
+        . . . f e f f f e f f e f . . . 
+        . . . f f . . f f . . f f . . . 
+        `,
+    img`
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 f . f d f 
+        . f 2 2 2 2 2 2 b b f f d f 
+        . f b d d d d d d b b d b f 
+        . f d d d d d b d d f f f . 
+        . f d f f f d f f d f . . . 
+        . f f . . f f . . f f . . . 
+        `,
+    img`
+        bbbb........bbbb.................
+        cffbb......bbffb.................
+        cfffbb....bbfffc.................
+        cfbffbccccbffbfc.................
+        cfbbffbccbffbbfc.................
+        ff3bffffffffb3fc.................
+        ff33ffffffff33fc.................
+        cff3ffffffff3ffc.................
+        cfffffff11fffffc.................
+        cfff1ff111ff1ffc........bbbbbb...
+        cfff1ff1111f1ffc.......cffffffbb.
+        cffff1111111fffc......cffffffffb.
+        cfff1111fff11ffc.....cfff111ffffb
+        cfffc11fff11ffbc.....cff11111fffb
+        cfffcc111111cfbc.....cf11dd111ffb
+        cfffffbb33ccffbcc....cbddbbd11ffc
+        cffffffb33cfffffbbccccbbdbbb11ffc
+        cfffffffbbffffffffffffffffbb1fffc
+        cffff1111fffffffffffffffffffbfffc
+        cfff111111fffffffffffffffffffffc.
+        cfff1111111fffffffffffffffffffcc.
+        cff11111111fffffffffffffffffffc..
+        cff111111111ffffffffffffffffffc..
+        cbf111111111ffffffffffffffffffc..
+        .ff111111111ffffffffffffffffffc..
+        .ff111111111ffffffffffffffffffc..
+        ..fb11111111ffffffffffffffffffc..
+        ...fb111111fffffff111fffffffffc..
+        ...fbbb1111ffffff11111111fffffc..
+        ....fbbfffbffffccccccccccbffffc..
+        ....fbbf..ffffc.....fbbf.cffffc..
+        ....fbbf..ffffc.....fbbf.ccffffc.
+        ....fbbf..fffc.......fbf..ccfffc.
+        ....fbbf..fffc.......fbbf..ccffc.
+        ....fbbf..fffc.......fbbf...cffc.
+        ....fbbf..fffc......fbbbf...cffc.
+        ...fbbbf..fffc......ffff....cbfc.
+        ...fbbf..ffffc.............cfffc.
+        ...ffff..fffcc.............cfffc.
+        .........fffc..............cccc..
+        `
+    ]
     scene.setBackgroundImage(img`
         bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
         bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
